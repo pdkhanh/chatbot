2d9d7190662caf6b6c3d2b7c6b5cff93d403320a
Khanh Pham <pdkhanh93@gmail.com>
2020-03-15 01:16:51 +0700

    .

--- server.js
+++ server.js
@@ -27,6 +27,9 @@ app.get('/webhook', function(req, res) { // Đây là path để validate tooken
 app.post('/webhook', function(req, res) { // Phần sử lý tin nhắn của người dùng gửi đến
 	console.log('Yah got message');
   var entries = req.body.entry;
+  console.log(req.body);
+  	console.log('Yah got message');
+  console.log(entries);
   for (var entry of entries) {
     var messaging = entry.messaging;
     for (var message of messaging) {
