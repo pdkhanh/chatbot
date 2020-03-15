58ff465bb2e816c5ebfec181a9930714ef055652
Khanh Pham <pdkhanh93@gmail.com>
2020-03-15 23:11:57 +0700

    .

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get('/webhook', function(req, res) { // Đây là path để validate tooken
   res.send('Error, wrong validation token');
 });
  
-app.post('/webhook', function(req, res) { // Phần sử lý tin nhắn của người dùng gửi đến
+app.post('/webhook1', function(req, res) { // Phần sử lý tin nhắn của người dùng gửi đến
 	var request = require('request');
 	request('https://code.junookyo.xyz/api/ncov-moh/data.json', function (error, response, body) {
 	  if (!error && response.statusCode == 200) {
@@ -41,11 +41,104 @@ app.post('/webhook', function(req, res) { // Phần sử lý tin nhắn của ng
 		
 		var senderId = req.body.entry[0].messaging[0].sender.id;
 		console.log(senderId)
-		sendMessage(senderId, returnBody);
-	
+		
+		setInterval(() => sendMessage(senderId, returnBody), 20000);
 	  }
 	})
 });
+
+
+app.post('/webhook3', function(req, res) { // Phần sử lý tin nhắn của người dùng gửi đến
+	var oldValue;
+	var ID = req.body.entry[0].messaging[0].sender.id;
+	var message = req.body.entry[0].messaging[0].message.text;
+	
+	if(message == "end"){
+		sendMessage(ID, "OK end");
+		res.status(200).send("OK end");
+		return;
+	}
+	
+	function check() {
+		getCorona1().then(function(body){
+			if (oldValue != body){
+				console.log("old value: " + JSON.stringify(oldValue));
+				console.log("new value: " + JSON.stringify(body));
+				oldValue = body;
+				
+				var jsonBody = JSON.parse(body);
+				var returnBody;
+				returnBody = "Tình hình Corona\n"
+							+ "Thế giới: " + jsonBody.data.global.cases + " ca nhiễm\n"
+							+ "Trong đó tử vong: " + jsonBody.data.global.deaths
+							+ ", hồi phục: " + jsonBody.data.global.recovered + "\n"
+							+ "---------------------------\n"
+							+ "Việt Nam: " + jsonBody.data.vietnam.cases + " ca nhiễm\n"
+							+ "Trong đó tử vong: " + jsonBody.data.vietnam.deaths
+							+ ", hồi phục: " + jsonBody.data.vietnam.recovered;
+				console.log(jsonBody.data);
+				res.status(200).send(returnBody);
+				
+				var senderId = req.body.entry[0].messaging[0].sender.id;
+				console.log(senderId)
+				sendMessage(senderId, returnBody)
+				
+			} else {
+				console.log("old value: " + JSON.stringify(oldValue));
+				console.log("new value: " + JSON.stringify(body));
+				console.log("equal");
+			}
+			
+		}).catch(function(error){
+			//do something on error
+		})
+	}
+	setInterval(() => check(), 40000);
+});
+
+var interval;
+app.post('/webhook', function(req, res) { // Phần sử lý tin nhắn của người dùng gửi đến
+	var oldValue;
+	var ID = req.body.entry[0].messaging[0].sender.id;
+	var message = req.body.entry[0].messaging[0].message.text;
+	
+	if(message == "end"){
+		sendMessage(ID, "OK end");
+		res.status(200).send("OK end");
+		clearInterval(interval);
+		return;
+	}
+	
+	function check() {
+		getTime().then(function(body){
+			if (oldValue != body){
+				console.log("old value: " + JSON.stringify(oldValue));
+				console.log("new value: " + JSON.stringify(body));
+				oldValue = body;
+				
+				var jsonBody = JSON.parse(body);
+				var returnBody = jsonBody.unixtime;
+
+				//res.status(200).send(returnBody);
+				
+				var senderId = req.body.entry[0].messaging[0].sender.id;
+				console.log(senderId)
+				sendMessage(senderId, returnBody)
+				
+			} else {
+				console.log("old value: " + JSON.stringify(oldValue));
+				console.log("new value: " + JSON.stringify(body));
+				console.log("equal");
+			}
+			
+		}).catch(function(error){
+			//do something on error
+		})
+	}
+	interval = setInterval(() => check(), 10000);
+});
+
+
 
 const https = require('https');
 
@@ -53,10 +146,27 @@ function getCorona(){
 	var request = require('request');
 	request('https://code.junookyo.xyz/api/ncov-moh/data.json', function (error, response, body) {
 	  if (!error && response.statusCode == 200) {
-		//console.log(body)
 		return body;
 	  }
 	})
+}
+
+function getTime() {
+    return new Promise(function (resolve, reject) {
+         request('http://worldtimeapi.org/api/timezone/America/Argentina/Salta', function(error, response, body) {
+            if (error) reject(error);
+            if (response.statusCode === 200) resolve(body);
+})
+});
+}
+
+function getCorona1() {
+    return new Promise(function (resolve, reject) {
+         request('https://code.junookyo.xyz/api/ncov-moh/data.json', function(error, response, body) {
+            if (error) reject(error);
+            if (response.statusCode === 200) resolve(body);
+})
+});
 }
 
 // Đây là function dùng api của facebook để gửi tin nhắn
